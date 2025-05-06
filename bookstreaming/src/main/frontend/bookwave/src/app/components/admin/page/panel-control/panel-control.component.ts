f1658a2029d7df88383187b528644d8198ac573b
Zahira <zrivrio2103@gmail.com>
2025-05-06 19:06:46 +0200

    panel de control uusarios

--- bookstreaming/src/main/frontend/bookwave/src/app/components/admin/page/panel-control/panel-control.component.ts
+++ bookstreaming/src/main/frontend/bookwave/src/app/components/admin/page/panel-control/panel-control.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { VistaUsuariosComponent } from '../../panel-control/vista-usuarios/vista-usuarios.component';
+import { VistaCarritoComponent } from '../../panel-control/vista-carrito/vista-carrito.component';
+import { VistaCategoriasComponent } from '../../panel-control/vista-categorias/vista-categorias.component';
+import { VistaLibrosComponent } from '../../panel-control/vista-libros/vista-libros.component';
+import { VistaListasComponent } from '../../panel-control/vista-listas/vista-listas.component';
+import { VistaProgresosComponent } from '../../panel-control/vista-progresos/vista-progresos.component';
+import { VistaReportesComponent } from '../../panel-control/vista-reportes/vista-reportes.component';
+import { VistaReviewsComponent } from '../../panel-control/vista-reviews/vista-reviews.component';
+
 @Component({
   selector: 'app-panel-control',
-  imports: [],
+  imports: [VistaUsuariosComponent, VistaCarritoComponent, VistaCategoriasComponent, VistaLibrosComponent, VistaListasComponent, VistaProgresosComponent, VistaReportesComponent, VistaReviewsComponent],
   templateUrl: './panel-control.component.html',
   styleUrl: './panel-control.component.css'
 })
--- bookstreaming/src/main/frontend/bookwave/src/app/components/admin/panel-control/vista-usuarios/vista-usuarios.component.ts
+++ bookstreaming/src/main/frontend/bookwave/src/app/components/admin/panel-control/vista-usuarios/vista-usuarios.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../../service/user.service';
+import { User } from '../../../../models/User';
 
 @Component({
   selector: 'app-vista-usuarios',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './vista-usuarios.component.html',
   styleUrl: './vista-usuarios.component.css'
 })
-export class VistaUsuariosComponent {
+export class VistaUsuariosComponent implements OnInit {
+  users: User[] = [];
+  isLoading: boolean = true;
+  error: string | null = null;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.isLoading = true;
+    this.userService.getAllUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error al cargar usuarios:', error);
+        this.error = 'Error al cargar los usuarios';
+        this.isLoading = false;
+      }
+    });
+  }
 }
